6f14eb6510c76e7ab9496dc521a72f8bb70869fc
LorincAkos <lorinc.akos13@gmail.com>
2024-02-25 05:18:39 +0100

    Base

--- ZombieGameMovement/Form1.Designer.cs
+++ ZombieGameMovement/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            txtammo = new Label();
+            txtscore = new Label();
+            txthealth = new Label();
+            healthBar = new ProgressBar();
+            player = new PictureBox();
+            GameTImer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            SuspendLayout();
+            // 
+            // txtammo
+            // 
+            txtammo.AutoSize = true;
+            txtammo.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txtammo.ForeColor = Color.White;
+            txtammo.Location = new Point(9, 9);
+            txtammo.Name = "txtammo";
+            txtammo.Size = new Size(92, 25);
+            txtammo.TabIndex = 0;
+            txtammo.Text = "Ammo: 0";
+            // 
+            // txtscore
+            // 
+            txtscore.AutoSize = true;
+            txtscore.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txtscore.ForeColor = Color.White;
+            txtscore.Location = new Point(367, 9);
+            txtscore.Name = "txtscore";
+            txtscore.Size = new Size(68, 25);
+            txtscore.TabIndex = 1;
+            txtscore.Text = "Kills: 0";
+            // 
+            // txthealth
+            // 
+            txthealth.AutoSize = true;
+            txthealth.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txthealth.ForeColor = Color.White;
+            txthealth.Location = new Point(664, 9);
+            txthealth.Name = "txthealth";
+            txthealth.Size = new Size(70, 25);
+            txthealth.TabIndex = 2;
+            txthealth.Text = "Health";
+            // 
+            // healthBar
+            // 
+            healthBar.Location = new Point(743, 14);
+            healthBar.Name = "healthBar";
+            healthBar.Size = new Size(169, 23);
+            healthBar.TabIndex = 3;
+            healthBar.Value = 100;
+            // 
+            // player
+            // 
+            player.Image = Properties.Resources.up;
+            player.Location = new Point(414, 287);
+            player.Name = "player";
+            player.Size = new Size(71, 100);
+            player.SizeMode = PictureBoxSizeMode.AutoSize;
+            player.TabIndex = 4;
+            player.TabStop = false;
+            // 
+            // GameTImer
+            // 
+            GameTImer.Enabled = true;
+            GameTImer.Interval = 20;
+            GameTImer.Tick += MainTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(924, 661);
+            Controls.Add(player);
+            Controls.Add(healthBar);
+            Controls.Add(txthealth);
+            Controls.Add(txtscore);
+            Controls.Add(txtammo);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label txtammo;
+        private Label txtscore;
+        private Label txthealth;
+        private ProgressBar healthBar;
+        private PictureBox player;
+        private System.Windows.Forms.Timer GameTImer;
     }
 }
--- ZombieGameMovement/Form1.cs
+++ ZombieGameMovement/Form1.cs
@@ -2,9 +2,137 @@ namespace ZombieGameMovement
 {
     public partial class Form1 : Form
     {
+
+        bool goUp, goDown, goLeft, goRight, gameOver;
+        string facing = "up";
+        int playerHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int zombieSpeed = 3;
+        int score = 0;
+
+        Random random = new Random();
+
+        List<PictureBox> zombiesList = new List<PictureBox>();
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (playerHealth > 1)
+            {
+                healthBar.Value = playerHealth;
+            }
+            else
+            {
+                gameOver = true;
+            }
+
+            txtammo.Text = "Ammo: " + ammo;
+            txtscore.Text = "Kills: " + score;
+
+            if (goLeft && player.Left > 0) 
+            {
+                player.Left -= speed;
+            }
+
+            if (goRight && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+
+            if(goUp && player.Top > 40)
+            {
+                player.Top -= speed;
+            }
+
+            if (goDown && player.Top +player.Height < this.ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode.Equals(Keys.Left))
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Properties.Resources.left;
+            }
+
+            if (e.KeyCode.Equals(Keys.Right))
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Properties.Resources.right;
+            }
+
+            if (e.KeyCode.Equals(Keys.Up))
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Properties.Resources.up;
+            }
+
+            if (e.KeyCode.Equals(Keys.Down))
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Properties.Resources.down;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Left))
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode.Equals(Keys.Right))
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode.Equals(Keys.Up))
+            {
+                goUp = false;
+            }
+
+            if (e.KeyCode.Equals(Keys.Down))
+            {
+                goDown = false;
+            }
+
+            if(e.KeyCode.Equals(Keys.Space))
+            {
+                ShootBullet(facing);
+            }
+        }
+
+        private void ShootBullet(string direction)
+        {
+            Bullet bullet = new Bullet();
+            bullet.direction = direction;
+            bullet.bulletLeft = player.Left + (player.Width /2); 
+            bullet.bulletTop = player.Top + (player.Height /2);
+            bullet.MakeBullet(this);
+        }
+
+        private void MakeZombies()
+        {
+
+        }
+
+        private void RestartGame()
+        {
+
         }
     }
 }
